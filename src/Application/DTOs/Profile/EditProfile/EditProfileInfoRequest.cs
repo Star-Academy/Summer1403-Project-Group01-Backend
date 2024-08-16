784e92ad3fccd69c1ff3a54692fac65db7e50c0a
amiralirahimii <amiralirahimi82@gmail.com>
2024-08-16 12:56:43 +0330

    refactor: change folder structure

--- src/Application/DTOs/Profile/EditProfile/EditProfileInfoRequest.cs
+++ src/Application/DTOs/Profile/EditProfile/EditProfileInfoRequest.cs
@@ -6,6 +6,4 @@ public class EditProfileInfoRequest
     public string UserName { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string OldPassword { get; set; } = string.Empty;
-    public string NewPassword { get; set; } = string.Empty;
 }
