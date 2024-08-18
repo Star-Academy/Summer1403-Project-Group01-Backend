a51a88df8e5e532e856af1cef3302dd8facd55cb
Mobin Barfi <m_barfi@comp.iust.ac.ir>
2024-08-18 20:27:06 +0330

    feat: Add get-all-transactions api

---
+++ src/Web/Mappers/TransactionMapper.cs
@@ -0,0 +1,20 @@
+﻿using Web.DTOs.Transaction;
+
+namespace Web.Mappers
+{
+    public static class TransactionMapper
+    {
+        public static List<GetAllTransactionsResponse.TransactionDto> ToGotAllTransactionsDto(this GetAllTransactionsResponse response)
+        {
+            return response.Transactions.Select(transaction => new GetAllTransactionsResponse.TransactionDto
+            {
+                TransactionId = transaction.TransactionId,
+                SourceAccountId = transaction.SourceAccountId,
+                DestinationAccountId = transaction.DestinationAccountId,
+                Amount = transaction.Amount,
+                Date = transaction.Date,
+                Type = transaction.Type
+            }).ToList();
+        }
+    }
+}
